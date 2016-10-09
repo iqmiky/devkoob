144caeb7305e52995c1cf7db63aef8e77d5df65c
Ibrahim Aladhamiy <iiqmikekh@gmail.com>
2016-10-09 23:21:24 +0000

    Added materialize

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery
+//= require turbolinks
+//= require materialize-sprockets
+//= require materialize/extras/nouislider
+
